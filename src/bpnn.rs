ee78614a7656166384c1540cb6005c693cefb2af
Nugine <Nugine@163.com>
2018-12-20 22:50:14 +0800

    add train_once

--- src/bpnn.rs
+++ src/bpnn.rs
@@ -4,8 +4,6 @@ mod utils;
 use self::types::*;
 use self::utils::*;
 
-use ndarray::Array;
-
 pub struct BPNN {
     layer_num: usize,
     weights: Vec<Matrix>,
@@ -47,5 +45,63 @@ impl BPNN {
             cost: cost,
             d_cost: d_cost,
         }
+    }
+
+    pub fn train_once(
+        &mut self,
+        input: &Vector,
+        target: &Vector,
+        rate: f64,
+        factor: f64,
+    ) -> (Vector, f64) {
+        let l = self.layer_num;
+        let mut W = &mut self.weights;
+        let mut C = &mut self.changes;
+        let activations = &self.activations;
+        let d_activations = &self.d_activations;
+
+        let mut a = vec![input.to_owned()];
+
+        for i in 0..l {
+            let x = &a[i];
+            let y = W[i].dot(x);
+            let act = (activations[i])(&y);
+            a.push(act)
+        }
+
+        let mut d = vec![{
+            let e = (self.d_cost)(target, &a[l]);
+            let da = (d_activations[l - 1])(&a[l]);
+            e * &da
+        }];
+
+        for i in 0..(l - 1) {
+            let j = l - 1 - i;
+            let e = W[j].t().dot(&d[i]);
+            let da = (d_activations[j - 1])(&a[j]);
+            d.push(e * &da)
+        }
+
+        d.reverse();
+
+        for i in 0..l {
+            W[i] += &(&C[i] * factor);
+        }
+
+        let output = a.pop().unwrap();
+
+        for i in (l - 1)..=0 {
+            let (ol, il) = C[i].dim();
+            let delta: Matrix = d.pop().unwrap().into_shape((ol, 1)).unwrap();
+            let ip: Matrix = a.pop().unwrap().into_shape((1, il)).unwrap();
+            C[i] = delta.dot(&ip);
+        }
+
+        for i in 0..l {
+            W[i] += &(&C[i] * rate);
+        }
+
+        let error = (self.cost)(target, &output);
+        (output, error)
     }
 }
