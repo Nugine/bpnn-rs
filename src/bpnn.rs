cccdf466382a1812b5c172edd21c29df0e421f8e
Nugine <Nugine@163.com>
2018-12-20 20:57:12 +0800

    add bpnn new

--- src/bpnn.rs
+++ src/bpnn.rs
@@ -1,2 +1,51 @@
 mod types;
 mod utils;
+
+use self::types::*;
+use self::utils::*;
+
+use ndarray::Array;
+
+pub struct BPNN {
+    layer_num: usize,
+    weights: Vec<Matrix>,
+    changes: Vec<Matrix>,
+    activations: Vec<Activation>,
+    d_activations: Vec<DActivation>,
+    cost: Cost,
+    d_cost: DCost,
+}
+
+impl BPNN {
+    pub fn new(
+        input_size: usize,
+        layer_settings: &Vec<(usize, Activation, DActivation)>,
+        cost: Cost,
+        d_cost: DCost,
+    ) -> Self {
+        let mut il = input_size + 1;
+        let mut Ws: Vec<Matrix> = Vec::new();
+        let mut Cs: Vec<Matrix> = Vec::new();
+        let mut acts: Vec<Activation> = Vec::new();
+        let mut d_acts: Vec<DActivation> = Vec::new();
+
+        for (ol, act, d_act) in layer_settings {
+            let ol = *ol;
+            Ws.push(random_matrix(ol, il));
+            Cs.push(zero_matrix(ol, il));
+            acts.push(*act);
+            d_acts.push(*d_act);
+            il = ol;
+        }
+
+        Self {
+            layer_num: layer_settings.len(),
+            weights: Ws,
+            changes: Cs,
+            activations: acts,
+            d_activations: d_acts,
+            cost: cost,
+            d_cost: d_cost,
+        }
+    }
+}
--- src/bpnn/utils.rs
+++ src/bpnn/utils.rs
@@ -3,7 +3,7 @@ use ndarray::Array;
 use rand::distributions::Uniform;
 use rand::{thread_rng, Rng};
 
-pub fn random_weight(row_size: usize, col_size: usize) -> Matrix {
+pub fn random_matrix(row_size: usize, col_size: usize) -> Matrix {
     let range = Uniform::new(-1., 1.);
     let v: Vec<f64> = thread_rng()
         .sample_iter(&range)
@@ -11,4 +11,8 @@ pub fn random_weight(row_size: usize, col_size: usize) -> Matrix {
         .collect();
 
     Array::from_shape_vec((row_size, col_size), v).unwrap()
+}
+
+pub fn zero_matrix(row_size: usize, col_size: usize) -> Matrix {
+    Array::zeros((row_size, col_size))
 }
